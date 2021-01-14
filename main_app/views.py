67b6aa8efc19b7cd4b8b6ac9acbc3bfdd02a7fb2
Amari James <mar.james1998@gmail.com>
2021-01-14 14:16:47 -0500

    notsure

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cats_index(request):
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
+
+def cats_show(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/show.html', { 'cat': cat })
